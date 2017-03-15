8cc4f07b787246ed9b6fc8afc1c5b421346e1550
Eri Irawan <eryirawan91@gmail.com>
2017-03-16 01:35:29 +0700

    intensiate routing for question

--- server/app.js
+++ server/app.js
@@ -7,16 +7,17 @@ const cors = require('cors')
 
 const app = express()
 
-// let index = require('./routes/index')
+let question = require('./routes/question')
 let user = require('./routes/user')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: false
+	extended: false
 }))
 app.use(morgan('tiny'))
 app.use(cors())
-// app.use('/api', index)
+
+app.use('/api', question)
 app.use('/api', user)
 
 app.listen(3000)
