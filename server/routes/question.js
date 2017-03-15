08765d8cf03ccfa951fb12c0148c21e04975760f
Eri Irawan <eryirawan91@gmail.com>
2017-03-16 01:37:57 +0700

    add endpoint for question

---
+++ server/routes/question.js
@@ -0,0 +1,11 @@
+'use strict'
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/questionController')
+
+router.get('/questions', controller.readQuestions)
+router.post('/qustion', controller.createQuestion)
+router.put('/question/:id', controller.updateQuestion)
+router.delete('/question/:id', controller.deleteQuestion)
+
+module.exports = router
