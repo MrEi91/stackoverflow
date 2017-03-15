8c5847207214726fad3e9258e95d515fa32eaf48
Eri Irawan <email": null>
2017-03-15 20:36:10 +0700

    add association to model question,user,vote

--- server/models/answer.js
+++ server/models/answer.js
@@ -1,15 +1,18 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
+'use strict'
+module.exports = function (sequelize, DataTypes) {
   var Answer = sequelize.define('Answer', {
     answer_content: DataTypes.STRING,
     QuestionId: DataTypes.INTEGER,
     UserId: DataTypes.INTEGER
   }, {
     classMethods: {
-      associate: function(models) {
+      associate: function (models) {
         // associations can be defined here
+        Answer.belongsTo(models.Question)
+        Answer.belongsTo(models.User)
+        Answer.hasMany(models.Vote)
       }
     }
-  });
-  return Answer;
-};
+  })
+  return Answer
+}
