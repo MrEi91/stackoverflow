3d81479bb21645366d16babf9e9a869acdd3778a
Eri Irawan <email": null>
2017-03-15 22:17:40 +0700

    add endpoint for login

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/userController')
 
 router.get('/users', controller.getUsers)
 router.post('/user/register', controller.register)
+router.post('/user/login', controller.login)
 
 module.exports = router
