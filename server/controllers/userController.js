4a95471a36311f51c405a846c6e3993e138098ce
Eri Irawan <email": null>
2017-03-15 22:16:24 +0700

    modify userController method login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,72 @@
 'use strict'
 
 const models = require('../models')
+const jwt = require('jsonwebtoken')
+const hash = require('password-hash')
+
+const secret = 'mrei91'
 
 let getUsers = (req, res, next) => {
   models.User.findAll().then((users) => {
     res.json(users)
   }).catch((e) => {
-    if (err) {
+    res.json({
+      message: e
+    })
+  })
+}
+
+let register = (req, res, next) => {
+  models.User.create(req.body).then((user) => {
+    if (user) {
       res.json({
-        message: err
+        message: 'SUCCESS REGISTER',
+        user: user
+      })
+    }
+  }).catch((e) => {
+    res.json({
+      error: e
+    })
+  })
+}
+
+let login = (req, res, next) => {
+  models.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then((user) => {
+    if (!user) {
+      res.json({
+        userNotFound: true
+      })
+    } else {
+      if (hash.verify(req.body.password, user.password)) {
+        let token = jwt.sign({
+          username: user.username
+        }, secret, {})
+
+        res.json({
+          token: token
+        })
+      } else {
+        res.json({
+          wrongPassword: true
+        })
+      }
+    }
+  }).catch((e) => {
+    if (e) {
+      req.json({
+        error: e
       })
     }
   })
 }
 
 module.exports = {
-  getUsers
+  getUsers,
+  register,
+  login
 }
