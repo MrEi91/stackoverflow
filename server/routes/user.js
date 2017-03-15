d290b8b4cd8666d2586f97aaa75b9081732a2cfd
Eri Irawan <email": null>
2017-03-15 22:29:50 +0700

    add endpoint for verify token

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const controller = require('../controllers/userController')
 
 router.get('/users', controller.getUsers)
+router.get('/user/verify/:token', controller.verifyToken)
 router.post('/user/register', controller.register)
 router.post('/user/login', controller.login)
 
