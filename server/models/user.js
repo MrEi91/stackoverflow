971c2d916e0fd9cd10bc56d3cef8259c6ba65c88
Eri Irawan <email": null>
2017-03-15 20:56:49 +0700

    add validation email,username and hooks with password hash

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,46 @@
 'use strict'
+var passwordHash = require('password-hash')
+
 module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        isUnique: function (value, next) {
+          User.find({
+            where: {
+              username: value
+            },
+            attributes: ['id']
+          }).done(function (user) {
+            if (user) {
+              return next('Username already in used')
+            }
+            next()
+          })
+        }
+      }
+    },
     fullname: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+        isUnique: function (value, next) {
+          User.find({
+            where: {
+              email: value
+            },
+            attributes: ['id']
+          }).done(function (user) {
+            if (user) {
+              return next('Email address already in used')
+            }
+            next()
+          })
+        }
+      }
+    },
     password: DataTypes.STRING
   }, {
     classMethods: {
@@ -12,6 +49,12 @@ module.exports = function (sequelize, DataTypes) {
         User.hasMany(models.Question)
         User.hasMany(models.Vote)
         User.hasMany(models.Answer)
+      }
+    },
+    hooks: {
+      beforeCreate: function (value, option) {
+        let hashed = passwordHash.generate(value)
+        password = hashed
       }
     }
   })
