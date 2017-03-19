3c4b0f23f1f59a88c87433ea867a6f3f2da7a99f
Eri Irawan <email": null>
2017-03-19 22:02:00 +0700

    add object methods

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -3,78 +3,93 @@
 const models = require('../models')
 // const slug = require('slug')
 
-let readQuestions = (req, res, next) => {
-	models.Question.findAll().then((questions) => {
-		res.send(questions)
-	}).catch((e) => {
-		res.send({
-			error: e
-		})
-	})
+let methods = {}
+
+methods.readQuestions = (req, res, next) => {
+  models.Question.findAll()
+    .then((questions) => {
+      res.send(questions)
+    })
+    .catch((e) => {
+      res.send({
+        error: e
+      })
+    })
 }
 
-let createQuestion = (req, res, next) => {
-	models.Question.create({
-		title: req.body.title,
-		question_content: req.body.question_content,
-		UserId: req.body.email,
-		// slug: slug(req.body.title).toLowerCase()
-	}).then((question) => {
-		res.send({
-			message: "CREATE NEW QUESTION SUCCESS!",
-			question: question
-		})
-	}).catch((e) => {
-		res.send({
-			error: e
-		})
-	})
+methods.createQuestion = (req, res, next) => {
+  models.Question.create({
+      title: req.body.title,
+      question_content: req.body.question_content,
+      UserId: req.body.email
+      // slug: slug(req.body.title).toLowerCase()
+    })
+    .then((question) => {
+      res.send({
+        message: 'CREATE NEW QUESTION SUCCESS!',
+        question: question
+      })
+    })
+    .catch((e) => {
+      res.send({
+        error: e
+      })
+    })
 }
 
-let updateQuestion = (req, res, next) => {
-	models.Question.findById(req.params.id).then((question) => {
-		if (!question) {
-			res.send({
-				message: "QUESTION IS NOT FOUND!"
-			})
-		} else {
-			question.update(req.body).then((result) => {
-				res.send({
-					message: "QUESTION HAS BEEN UPDATED!",
-					question: result
-				})
-			})
-		}
-	}).catch((e) => {
-		res.send({
-			error: e
-		})
-	})
+methods.updateQuestion = (req, res, next) => {
+  models.Question.findById(req.params.id)
+    .then((question) => {
+      if (!question) {
+        res.send({
+          message: 'QUESTION IS NOT FOUND!'
+        })
+      } else {
+        question.update(req.body)
+          .then((result) => {
+            res.send({
+              message: 'QUESTION HAS BEEN UPDATED!',
+              question: result
+            })
+          })
+          .catch((err) => {
+            res.send({
+              error: error
+            })
+          })
+      }
+    })
+    .catch((e) => {
+      res.send({
+        error: e
+      })
+    })
 }
 
-let deleteQuestion = (req, res, next) => {
-	models.Question.findById(req.params.id).then((question) => {
-		if (!question) {
-			res.send({
-				message: "QUESTION IS NOT FOUND!"
-			})
-		} else {
-			question.destroy({
-				where: {
-					id: req.params.id
-				}
-			})
-		}
-	}).catch((e) => {
-		res.send({
-			error: e
-		})
-	})
+methods.deleteQuestion = (req, res, next) => {
+  models.Question.findById(req.params.id)
+    .then((question) => {
+      if (!question) {
+        res.send({
+          message: 'QUESTION IS NOT FOUND!'
+        })
+      } else {
+        question.destroy({
+          where: {
+            id: req.params.id
+          }
+        }).catch((error) => {
+          res.send({
+            error: error
+          })
+        })
+      }
+    })
+    .catch((e) => {
+      res.send({
+        error: e
+      })
+    })
 }
 
-module.exports = {
-	createQuestion,
-	readQuestions,
-	updateQuestion,
-	deleteQuestion
-}
+module.exports = methods
