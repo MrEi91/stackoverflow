88503372ff4ca20e943e0f80eadc81c6803def2f
Eri Irawan <email": null>
2017-03-20 21:19:26 +0700

    Include model Question, Vote and Vote

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,91 +7,99 @@ const hash = require('password-hash')
 const secret = 'mrei91'
 
 let getUsers = (req, res, next) => {
-	models.User.findAll().then((users) => {
-		if (!users) {
-			res.send({
-				message: "Model users is empty"
-			})
-		} else {
-			res.json(users)
-		}
-	}).catch((e) => {
-		res.json({
-			message: "Table user is not found!",
-			error: e
-		})
-	})
+  models.User.findAll({
+    include: [{
+      model: models.Question
+    }, {
+      model: models.Vote
+    }, {
+      model: models.Answer
+    }]
+  }).then((users) => {
+    if (!users) {
+      res.send({
+        message: 'Model users is empty'
+      })
+    } else {
+      res.json(users)
+    }
+  }).catch((e) => {
+    res.json({
+      message: 'Table user is not found!',
+      error: e
+    })
+  })
 }
 
 let register = (req, res, next) => {
-	models.User.create(req.body).then((user) => {
-		if (user) {
-			res.json({
-				message: 'SUCCESS REGISTER',
-				user: user
-			})
-		}
-	}).catch((e) => {
-		res.json({
-			error: e
-		})
-	})
+  models.User.create(req.body).then((user) => {
+    if (user) {
+      res.json({
+        message: 'SUCCESS REGISTER',
+        user: user
+      })
+    }
+  }).catch((e) => {
+    res.json({
+      error: e
+    })
+  })
 }
 
 let login = (req, res, next) => {
-	models.User.findOne({
-		where: {
-			username: req.body.username
-		}
-	}).then((user) => {
-		if (!user) {
-			res.json({
-				userNotFound: true
-			})
-		} else {
-			if (hash.verify(req.body.password, user.password)) {
-				let token = jwt.sign({
-					username: user.username
-				}, secret, {})
+  models.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then((user) => {
+    if (!user) {
+      res.json({
+        userNotFound: true
+      })
+    } else {
+      if (hash.verify(req.body.password, user.password)) {
+        let token = jwt.sign({
+          username: user.username
+        }, secret, {})
 
-				res.json({
-					token: token
-				})
-			} else {
-				res.json({
-					wrongPassword: true
-				})
-			}
-		}
-	}).catch((e) => {
-		if (e) {
-			req.json({
-				error: e
-			})
-		}
-	})
+        res.json({
+          token: token
+        })
+      } else {
+        res.json({
+          wrongPassword: true
+        })
+      }
+    }
+  }).catch((e) => {
+    if (e) {
+      req.json({
+        error: e
+      })
+    }
+  })
 }
 
 let verifyToken = (req, res) => {
-	let params = req.params.token
-	jwt.verify(params, secret, (err, decoded) => {
-		if (err) console.log(err)
+  let params = req.params.token
+  jwt.verify(params, secret, (err, decoded) => {
+    if (err) console.log(err)
 
-		if (!decoded) {
-			res.json({
-				username: false
-			})
-		} else {
-			res.json({
-				username: true
-			})
-		}
-	})
+    if (!decoded) {
+      res.json({
+        username: false
+      })
+    } else {
+      res.json({
+        username: true
+      })
+    }
+  })
 }
 
 module.exports = {
-	getUsers,
-	register,
-	login,
-	verifyToken
+  getUsers,
+  register,
+  login,
+  verifyToken
 }
