92e52172d8cbec0b2c92b71f0c85d71270b8fb80
Eri Irawan <email": null>
2017-03-20 21:20:13 +0700

    include model User and Answer

--- server/controllers/voteController.js
+++ server/controllers/voteController.js
@@ -5,47 +5,53 @@ const models = require('../models')
 let methods = {}
 
 methods.getVotes = (req, res, next) => {
-	models.Vote.findAll()
-		.then((votes) => {
-			res.send(votes)
-		})
-		.catch((err) => {
-			res.send(err)
-		})
+  models.Vote.findAll({
+    include: [{
+      model: models.Answer
+    }, {
+      model: models.User
+    }]
+  })
+    .then((votes) => {
+      res.send(votes)
+    })
+    .catch((err) => {
+      res.send(err)
+    })
 }
 
 methods.createVote = (req, res, next) => {
-	models.Vote.create({
-			vote_count: req.body.vote_count,
-			AnswerId: req.body.AnswerId,
-			UserId: req.body.UserId
-		})
-		.then((vote) => {
-			res.send(vote)
-		})
-		.catch((err) => {
-			res.send(err)
-		})
+  models.Vote.create({
+    vote_count: req.body.vote_count,
+    AnswerId: req.body.AnswerId,
+    UserId: req.body.UserId
+  })
+    .then((vote) => {
+      res.send(vote)
+    })
+    .catch((err) => {
+      res.send(err)
+    })
 }
 
 methods.updateVote = (req, res, next) => {
-	models.Vote.findById(req.params.id)
-		.then((vote) => {
-			vote.update({
-					vote_count: req.body.vote_count,
-					AnswerId: req.body.AnswerId,
-					UserId: req.body.UserId
-				})
-				.then((vote) => {
-					req.send(vote)
-				})
-				.catch((err) => {
-					res.send(err)
-				})
-		})
-		.catch((err) => {
-			res.send(err)
-		})
+  models.Vote.findById(req.params.id)
+    .then((vote) => {
+      vote.update({
+        vote_count: req.body.vote_count,
+        AnswerId: req.body.AnswerId,
+        UserId: req.body.UserId
+      })
+        .then((vote) => {
+          req.send(vote)
+        })
+        .catch((err) => {
+          res.send(err)
+        })
+    })
+    .catch((err) => {
+      res.send(err)
+    })
 }
 
 module.exports = methods
